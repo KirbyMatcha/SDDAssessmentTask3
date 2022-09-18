ea2ff6d6bde3f1a09794c67e5f70dca85f9675e5
pigeony105 <alice.li17@education.nsw.gov.au>
2022-09-18 23:15:43 +1000

    adgf

---
+++ SDDAssessmentTask3/Globals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDDAssessmentTask3
+{
+    public static class Globals
+    {
+        public static List<Progress> progresses = new List<Progress>();
+    }
+}
---
+++ SDDAssessmentTask3/Score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDDAssessmentTask3
+{
+    public class Progress
+    {
+        public int answered1;
+        public int answeredCorrect1;
+        public string name;
+        public Progress(int _answered1, int _answeredCorrect1, string _name)
+        {
+            answered1 = _answered1;
+            answeredCorrect1 = _answeredCorrect1;
+            name = _name;
+        }
+        public string reportMe()
+        {
+            return "My score is  " + answeredCorrect1 + " out of " + answered1 + " and my name is " + name;
+        }
+    }
+}
